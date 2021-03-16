da4ed4be42863ee0700e3f27465c637e77094440
Thiez-64 <ma.ojeanson@gmail.com>
2021-03-16 15:05:52 +0100

    Top navbar has been modified

--- script.js
+++
@@ -1,4 +0,0 @@
-const bouttonBurger = document.querySelectorAll('.burger');
-const navSlider = document.querySelector('.nav-slider');
-const buttonX = document.querySelector('.nav-slider-menu-x');
-
