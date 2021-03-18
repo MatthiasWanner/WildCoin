25104a33e5f598261f9225967ddd51fd4869d2e3
Thiez-64 <ma.ojeanson@gmail.com>
2021-03-18 12:36:22 +0100

    Burger menu has been placed correctly and the cinematic is working good for mobile

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,27 @@
-const buttonBurger = document.querySelector('.burger');
+const buttonBurger = document.querySelector('#burger');
 const navMenu = document.querySelector('#nav-menu');
-const buttonBurgerX = document.querySelector('.burger.active');
 
 buttonBurger.addEventListener('click', () => {
-    buttonBurger.classList.toggle('active');
+    buttonBurger.classList.toggle('pivot');
     navMenu.classList.toggle('nav-menu-animation');
 });
 
+// buttonBurger.addEventListener('mouseover', () => {
+//     buttonBurger.classList.toggle('animate-burger');
+// });
 
+// buttonBurger.addEventListener('mouseleave', () => {
+//     buttonBurger.classList.toggle('animate-burger');
+// });
+
+
+
+// const animateText = document.querySelector('.footer-content');
+
+// animateText.addEventListener('mouseover', () => {
+//     animateText.classList.toggle('animate-footer');
+// });
+
+// animateText.addEventListener('mouseleave', () => {
+//     animateText.classList.toggle('animate-footer');
+// });
