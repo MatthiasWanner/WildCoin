4517afef40102ed89c60281b3ee5909141ed3800
MatthiasWanner <matthias.wanner@gmail.com>
2021-03-19 14:41:34 +0100

    Ready to first presentation

--- assets/js/items.js
+++ assets/js/items.js
@@ -3,27 +3,27 @@ const team = [
         name: "Julien",
         picture: "../assets/img/man-6077947_960_720.webp",
         job: "Front-end Dev",
-        description: "Pastry muffin tiramisu toffee caramels.lorem Pastry muffin tiramisu toffee caramels.lorem Pastry muffin tiramisu toffee caramels.lorem"
+        description: "Candy canes gummi bears cake jelly beans gingerbread gingerbread toffee. Tiramisu macaroon chocolate bar sweet sesame snaps jelly cheesecake icing. Tiramisu sugar plum fruitcake cake halvah dessert. Jelly bonbon marzipan sweet roll bonbon pie cheesecake chocolate chocolate. Chupa chups pie jelly-o brownie marshmallow halvah chocolate bar wafer dessert. Tootsie roll brownie gummi bears. Chupa chups cookie pastry topping jelly beans. Bear claw sweet caramels cookie tootsie roll jelly beans lollipop."
     },
 
     {
         name: "Matthieu",
         picture: "../assets/img/man-6077947_960_720.webp",
         job: "Front-end Dev",
-        description: "Pastry muffin tiramisu toffee caramels marzipan."
+        description: "Jelly beans jelly-o cotton candy chupa chups croissant cotton candy bonbon chocolate candy. Soufflé gingerbread jelly tiramisu pie pie tart. Cotton candy cotton candy carrot cake cake bear claw apple pie cookie sweet. Candy dragée sesame snaps lollipop. Ice cream marzipan jelly-o carrot cake. Pudding gummies chupa chups pie pie dessert biscuit. Cookie marshmallow chocolate icing marzipan carrot cake halvah."
     },
 
     {
         name: "Matthias",
         picture: "../assets/img/man-6077947_960_720.webp",
         job: "Front-end Dev",
-        description: "Pastry muffin tiramisu toffee caramels."
+        description: "Cheesecake jelly-o caramels carrot cake gummies liquorice brownie. Bear claw oat cake chocolate cake tiramisu soufflé apple pie sesame snaps. Wafer ice cream cheesecake sesame snaps sweet roll chupa chups sugar plum marshmallow. Lollipop cheesecake jelly-o cookie dessert fruitcake chocolate bar powder soufflé. Dessert sweet roll caramels ice cream jelly beans sugar plum cookie. Liquorice oat cake bear claw chocolate bar gummi bears. Jujubes pie tootsie roll caramels lemon drops chupa chups apple pie. Chocolate cake brownie gummies liquorice pie marzipan jelly beans jujubes."
     },
 
     {
         name: "Mickael",
         picture: "../assets/img/man-6077947_960_720.webp",
         job: "Front-end Dev",
-        description: "Pastry muffin tiramisu toffee caramels marzipan soufflé wafer."
+        description: "Cotton candy gummi bears gingerbread. Apple pie sugar plum ice cream icing lemon drops chupa chups gummies tootsie roll chocolate cake. Icing marshmallow apple pie wafer. Bonbon cake cake ice cream croissant dessert. Jelly-o cake cake jujubes cake chocolate cupcake. Sweet chocolate fruitcake lollipop gummi bears pie carrot cake oat cake lemon drops. Brownie sweet croissant cheesecake ice cream jujubes jelly dragée cotton candy. Soufflé dessert candy canes."
     },
 ];
