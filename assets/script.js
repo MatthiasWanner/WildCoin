689d04f72f24441b474c563c82082f222aaba8dd
Thiez-64 <ma.ojeanson@gmail.com>
2021-03-21 21:08:17 +0100

    Last saved before git merge

--- assets/script.js
+++ assets/script.js
@@ -25,3 +25,24 @@ buttonBurger.addEventListener('click', () => {
 // animateText.addEventListener('mouseleave', () => {
 //     animateText.classList.toggle('animate-footer');
 // });
+
+let sun = document.getElementById('sun');
+let moon = document.getElementById('moon');
+
+
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        moon.style.opacity = "0";
+        sun.style.opacity = "0.5"
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        sun.style.opacity = "0";
+        moon.style.opacity = "0.5";
+    }
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
