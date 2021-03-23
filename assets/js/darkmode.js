e6752f85f57ac83390589c8aceef40a6e8fa6587
Thiez-64 <ma.ojeanson@gmail.com>
2021-03-23 14:46:55 +0100

    Darkmode has been fixed on each pages

--- assets/js/darkmode.js
+++ assets/js/darkmode.js
@@ -1,20 +1,25 @@
 let sun = document.getElementById('sun');
 let moon = document.getElementById('moon');
 
+const swapTheme = (e) => {
+    switchTheme(e)
+	const theme = document.documentElement.classList;
+
+	theme.toggle('theme-dark');
+	theme.toggle('theme-light');
+};
 
 const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
 
 function switchTheme(e) {
     if (e.target.checked) {
-        document.documentElement.setAttribute('data-theme', 'dark');
-        moon.style.opacity = "0";
-        sun.style.opacity = "0.5"
+        moon.style.opacity = "1";
+        sun.style.opacity = "0"
     }
     else {
-        document.documentElement.setAttribute('data-theme', 'light');
-        sun.style.opacity = "0";
-        moon.style.opacity = "0.5";
+        sun.style.opacity = "1";
+        moon.style.opacity = "0";
     }    
 }
 
-toggleSwitch.addEventListener('change', switchTheme, false);
+toggleSwitch.addEventListener('change', swapTheme, false);
--- assets/js/homepage.js
+++
@@ -1,6 +0,0 @@
-const swapTheme = () => {
-	const theme = document.documentElement.classList;
-
-	theme.toggle('theme-dark');
-	theme.toggle('theme-light');
-};
