d8b91349dad530f4d61b35c8a808b8a95335d01e
JIdayyy <julienabbadie1@gmail.com>
2021-03-25 09:08:32 +0100

    Multi Fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,9 +8,3 @@ buttonBurger.addEventListener('click', () => {
 
 
 
-let buttonSubmit = document.getElementById('button')
-console.log(buttonSubmit)
-buttonSubmit.addEventListener('click', function(e){
-    buttonSubmit.preventDefault();
-})
-
