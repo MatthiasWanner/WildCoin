e70596926ec8207860dd6c5a34bddf455dbbd327
Aeryle <47262222+Aeryle@users.noreply.github.com>
2021-03-26 13:43:52 +0100

    🐛 FIX: Table width

--- assets/js/table.js
+++ assets/js/table.js
@@ -163,49 +163,49 @@ const myData = async () => {
 
 async function updateEthereumPrice() {
 	let { times, prices } = await ethereumData();
-	let currentPrice = prices[prices.length - 1].toFixed(0);
+	let currentPrice = prices[prices.length - 1].toFixed(2);
 
 	document.getElementById('ethPrice').innerHTML = `${symbol}` + currentPrice;
 }
 
 async function updateXrpPrice() {
 	let { times, prices } = await xrpData();
-	let currentPrice = prices[prices.length - 1].toFixed(0);
+	let currentPrice = prices[prices.length - 1].toFixed(2);
 
 	document.getElementById('xrpPrice').innerHTML = `${symbol}` + currentPrice;
 }
 
 async function updateCosmosPrice() {
 	let { times, prices } = await cosmosData();
-	let currentPrice = prices[prices.length - 1].toFixed(0);
+	let currentPrice = prices[prices.length - 1].toFixed(2);
 
 	document.getElementById('atomPrice').innerHTML = `${symbol}` + currentPrice;
 }
 
 async function updateBitcoinPrice() {
 	let { times, prices } = await btcData();
-	let currentPrice = prices[prices.length - 1].toFixed(0);
+	let currentPrice = prices[prices.length - 1].toFixed(2);
 
 	document.getElementById('btcPrice').innerHTML = `${symbol}` + currentPrice;
 }
 
 async function updateLtcPrice() {
 	let { times, prices } = await ltcData();
-	let currentPrice = prices[prices.length - 1].toFixed(0);
+	let currentPrice = prices[prices.length - 1].toFixed(2);
 
 	document.getElementById('ltcPrice').innerHTML = `${symbol}` + currentPrice;
 }
 
 async function updateBnbPrice() {
 	let { times, prices } = await bnbData();
-	let currentPrice = prices[prices.length - 1].toFixed(0);
+	let currentPrice = prices[prices.length - 1].toFixed(2);
 
 	document.getElementById('bnbPrice').innerHTML = `${symbol}` + currentPrice;
 }
 
 async function updateMyPrice() {
 	let { times, prices } = await myData();
-	let currentPrice = prices[prices.length - 1].toFixed(0);
+	let currentPrice = prices[prices.length - 1].toFixed(2);
 
 	document.getElementById('myPrice').innerHTML = `${symbol}` + currentPrice;
 }
